0d33a2f416e35a993b11ca41a3c534214013a7a5
nmue4xf <jamkhindikarsantosh@johndeere.com>
2024-10-20 01:15:15 +0530

    Final Working

---
+++ emailservice/src/main/java/com/net/javaguide/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,27 @@
+package com.net.javaguide.emailservice.kafka;
+
+import com.net.javaguide.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topi.name}"
+            , groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent order) {
+        logger.info(String.format("#### -> Consumed message -> %s", order.toString()));
+        logger.info(String.format("#### -> Consumed message -> %s", order.getOrder().toString()));
+        logger.info(String.format("#### -> Consumed message -> %s", order.getStatus()));
+        logger.info(String.format("#### -> Consumed message -> %s", order.getMessage()));
+
+
+
+
+    }
+}
